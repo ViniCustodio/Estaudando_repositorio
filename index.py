4ad044273c6a6c3fb07d09bf81dd16d913fc0c34
Vinicius Custodio <vinicius.rcf88@gmail.com>
2023-04-21 20:58:56 -0300

    Adicionei uma logica para identificar se vc vota esse ano

--- index.py
+++ index.py
@@ -4,3 +4,12 @@ while p < 11:
     t = n * p
     print("{} x {} = {}".format(n, p, t))
     p = p + 1
+
+i = int(input("Qual seu ano de nascimento? "))
+a = 2023
+v = a - i
+
+if v >= 18:
+    print("Você tem {} anos ou mais, então você pode votar esse ano!".format(v))
+else: 
+    print("Você tem {} anos de idade, isso significa que você é menor e não pode votar esse ano. :C".format(v))
