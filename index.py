e88db9a45b00566e41a5c2966ec11cbc940c8c38
Vinicius Custodio <vinicius.rcf88@gmail.com>
2023-04-21 21:17:40 -0300

    Adicionado lógica de repetição para comparação de senha

--- index.py
+++ index.py
@@ -8,8 +8,15 @@ while p < 11:
 i = int(input("Qual seu ano de nascimento? "))
 a = 2023
 v = a - i
-
 if v >= 18:
     print("Você tem {} anos ou mais, então você pode votar esse ano!".format(v))
 else: 
     print("Você tem {} anos de idade, isso significa que você é menor e não pode votar esse ano. :C".format(v))
+
+r = int(input("Digite sua senha: "))
+s = int(1234)
+while r != s:
+    print("Senha incorreta, tente novamente: ")
+    r = int(input("Digite sua senha: "))
+print("ACESSO PERMITIDO! :D")
+        
